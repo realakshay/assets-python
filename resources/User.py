68283f84d92d3eeaa9131bf0ebb3890195b0dfc1
realakshay <45968153+realakshay@users.noreply.github.com>
2021-03-23 13:45:10 +0530

    minor changes

--- resources/User.py
+++ resources/User.py
@@ -59,8 +59,7 @@ class UserLogin(Resource):
                 # if data.password == json_data['password']:
                     access_token = create_access_token(identity=data.id, fresh=True)
                     user_data = {"role": data.role, "id":data.id, "firstName": data.firstName}
-                    return { 
-                        "Message": "LOGIN_SUCCESS", "data": user_data, "access_token":access_token}, 201
+                    return {"Message": "LOGIN_SUCCESS", "data": user_data, "access_token":access_token}, 201
                 else :
                     return {"Message" : "INCORRECT_PASSWORD"}, 403
             return {"Message" : "USER_NOT_ACTIVATED_YET"}, 401
